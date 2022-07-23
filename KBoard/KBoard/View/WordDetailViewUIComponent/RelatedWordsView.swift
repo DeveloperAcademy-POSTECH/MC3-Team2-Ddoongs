0d933e2196b3137c8a4dd7780526147f78eaa9ea
sunshiningsoo <yenkee1219@gmail.com>
2022-07-24 01:03:22 +0900

    Feat: "Related words의 간단한 UI 구현"
    Related to: #7 , #11
    
    Related words의 간단한 셀과 간단한 버튼 UI 구현
    
    TODO:
    1. 간단한 UI이기 때문에, 동적으로 height 변화가 안되고 있음
    2. related word가 늘어남에 따라서 동적으로 버튼들을 배치해주는 로직이 안짜여 있음

---
+++ KBoard/KBoard/View/WordDetailViewUIComponent/RelatedWordsView.swift
@@ -0,0 +1,60 @@
+//
+//  RelatedWordsView.swift
+//  KBoard
+//
+//  Created by 박성수 on 2022/07/23.
+//
+
+import UIKit
+
+class RelatedWordsView: UIView {
+    
+    // MARK: - property
+    private let relatedWordsLabel: UILabel = {
+        let relatedWordsLabel = UILabel()
+        relatedWordsLabel.text = "Related Words"
+        relatedWordsLabel.font = .boldSystemFont(ofSize: 16)
+        return relatedWordsLabel
+    }()
+    
+    private let relatedWordButton: UIButton = {
+        let relatedWordButton = UIButton()
+        relatedWordButton.setTitle("Inki-Gayo", for: .normal)
+        relatedWordButton.titleLabel?.font = .boldSystemFont(ofSize: 17)
+        relatedWordButton.tintColor = .white
+        relatedWordButton.backgroundColor = .systemBlue
+        relatedWordButton.layer.cornerRadius = 10
+        relatedWordButton.contentEdgeInsets = UIEdgeInsets(top: 8, left: 15, bottom: 8, right: 15)
+        return relatedWordButton
+    }()
+
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func render() {
+        self.addSubview(relatedWordsLabel)
+        relatedWordsLabel.anchor(top: self.topAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
+        
+        self.addSubview(relatedWordButton)
+        relatedWordButton.anchor(top: relatedWordsLabel.bottomAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, paddingTop: 10, paddingLeft: 20, paddingBottom: 20)
+        
+    }
+    
+    private func configureUI() {
+        self.backgroundColor = .systemBackground
+        self.layer.cornerRadius = 15
+        self.layer.masksToBounds = false
+        self.layer.shadowRadius = 5
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        self.layer.shadowOpacity = 0.2
+    }
+    
+}
