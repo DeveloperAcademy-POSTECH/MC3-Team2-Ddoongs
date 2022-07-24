ba1bc92726476835d308fb22379fb04508036403
sunshiningsoo <yenkee1219@gmail.com>
2022-07-25 02:31:09 +0900

    Feat: "WordDetailViewController에서 뷰 이어주기"
    만들어준 뷰객체들 ViewController에서 autolayout으로 이어주기
    
    Resolved: #11

--- KBoard/KBoard/Controller/WordDetailViewController.swift
+++ KBoard/KBoard/Controller/WordDetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class WordDetailViewController: UIViewController {
     
     private let wordDescriptionView = WordDescriptionView()
+    private let wordUsageView = WordUsageView()
+    private let relatedWordsView = RelatedWordsView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,12 @@ class WordDetailViewController: UIViewController {
         view.addSubview(wordDescriptionView)
         wordDescriptionView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
         wordDescriptionView.centerX(inView: view)
+        
+        view.addSubview(wordUsageView)
+        wordUsageView.anchor(top: wordDescriptionView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
+        
+        view.addSubview(relatedWordsView)
+        relatedWordsView.anchor(top: wordUsageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
         
     }
     
