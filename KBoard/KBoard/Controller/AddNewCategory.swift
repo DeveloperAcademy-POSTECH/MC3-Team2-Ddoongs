c5ee64c2fa3b2a6bbc567ec02d189df12cfec2bd
GODNOEL <103012104+GODNOEL@users.noreply.github.com>
2022-07-26 13:54:14 +0900

    Feat: 깃오류로 인한 작업 파일 재 업로드 업로드 파일 Controller - AddNewCategory -CategoryListViewController -CustomTableCell
    Model
    - KBoardData

---
+++ KBoard/KBoard/Controller/AddNewCategory.swift
@@ -0,0 +1,57 @@
+//
+//  AddNewCategory.swift
+//  KBoard
+//
+//  Created by Yu ahyeon on 2022/07/21.
+//
+
+import UIKit
+
+class AddNewCategory: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Properties
+    lazy var categoryTextField: UITextField = {
+        let categoryTextField = UITextField()
+        categoryTextField.placeholder = "enter New Category Name!"
+        categoryTextField.delegate = self
+        categoryTextField.clearButtonMode = .whileEditing
+        categoryTextField.borderStyle = .none
+        return categoryTextField
+    }()
+    
+    // MARK: - Lifecycle
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        setCategoryNameInput()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = UIColor.white
+        self.navigationItem.title = "Category Name"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.rightBarButtonItem =
+        UIBarButtonItem.init(title: "Save", style: .done, target: self, action: #selector(addNewCategory(_:)))
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "Cancel", style: .done, target: self, action: #selector(dismissModal(_:)))
+    }
+    
+    private func setCategoryNameInput() {
+        view.addSubview(categoryTextField)
+        categoryTextField.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 20, paddingLeft: 16, paddingRight: 16)
+    }
+    
+    @objc private func addNewCategory(_ sender: Any) {
+        // TO-Do : 텍스트 필드에 입력한 텍스트로 해당 카테고리 명 변경
+//        guard let newCategoryName = categoryTextField.text else { return }
+//        if !newCategoryName.isEmpty {
+//            CategoryListViewController.categories.append(Category(categoryName: newCategoryName, count: nil, words: nil))
+        }
+    
+    @objc private func dismissModal(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ KBoard/KBoard/Controller/CustomTableCell.swift
@@ -0,0 +1,89 @@
+//
+//  CustomTableCell.swift
+//  KBoard
+//
+//  Created by Yu ahyeon on 2022/07/21.
+//
+
+import UIKit
+
+// MARK: - MoreButton Action
+protocol CategoryEditDelegate: AnyObject {
+    func tapMoreButton()
+}
+
+// MARK: - UITableViewCell
+class CustomTableCell: UITableViewCell {
+    
+    // MARK: - Properties
+    var cellDelegate: CategoryEditDelegate?
+    
+    var categoryInfo: Category? {
+        didSet {configure()}
+    }
+    
+    lazy var categoryName: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        return label
+    }()
+    
+    lazy var categoryWordsCount: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    lazy var moreButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "ellipsis"), for: .normal)
+        button.tintColor = UIColor.black
+        button.addTarget(self, action: #selector(tapMoreButton), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    // 코드로 cell을 만들면 Init을 해줘야함
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setCellConstraint()
+        self.moreButton.addTarget(self, action: #selector(tapMoreButton), for: .touchUpInside)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOpacity = 0.1
+        self.layer.shadowRadius = 5
+        self.layer.masksToBounds = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setCellConstraint() {
+        let hstack = UIStackView(arrangedSubviews: [categoryName, moreButton])
+        hstack.axis = .horizontal
+        let vstack = UIStackView(arrangedSubviews: [hstack, categoryWordsCount])
+        vstack.axis = .vertical
+        vstack.spacing = 40
+        self.contentView.addSubview(vstack)
+        vstack.anchor(top: contentView.topAnchor, left: contentView.leftAnchor, bottom: contentView.bottomAnchor, right: contentView.rightAnchor, paddingTop: 15, paddingLeft: 20, paddingBottom: 15, paddingRight: 20)
+    }
+    
+    func configure() {
+        guard let categoryInfo = categoryInfo else {return}
+        categoryName.text = categoryInfo.categoryName
+        categoryWordsCount.text = categoryInfo.count
+    }
+    
+    @objc func tapMoreButton() {
+        cellDelegate?.tapMoreButton()
+    }
+}
