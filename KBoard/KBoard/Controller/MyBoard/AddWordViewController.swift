409eeceea2a012604fa1a2be6bcced10ba1e85e1
sunshiningsoo <yenkee1219@gmail.com>
2022-07-30 15:55:34 +0900

    Feat: 단어명 같은 단어 추가할시, Alert
    Resolves: #56

--- KBoard/KBoard/Controller/MyBoard/AddWordViewController.swift
+++ KBoard/KBoard/Controller/MyBoard/AddWordViewController.swift
@@ -107,12 +107,18 @@ class AddWordViewController: UIViewController {
         navigationItem.title = "Add word"
     }
     
-    @objc func dismissPage() {
+    @objc private func dismissPage() {
         // TODO: Save 하는 함수를 넣어야 한다.
-        dismiss(animated: true, completion: nil)
+//        if 중복되는 단어명을 찾는 로직 {
+//            let alert = UIAlertController(title: "Can't Add your New Word", message: "This word already in your Board!!\n Please try another one", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+//            present(alert, animated: true)
+//        } else {
+            dismiss(animated: true, completion: nil)
+//        }
     }
     
-    @objc func doneTapped() {
+    @objc private func doneTapped() {
         self.view.endEditing(true)
     }
     
